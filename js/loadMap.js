bb46b987c9403d4e3402910ce860d6d504e278c3
Sunia97 <sonia.arroyo.2015@salleurl.edu>
2017-12-15 14:58:05 +0100

    Avançant ajax

--- js/loadMap.js
+++ js/loadMap.js
@@ -1,28 +1,28 @@
+var token = "fa8a1b5b-55ff-4f03-b5f9-be0a95e713e2";
+var slot = "nueva";
+
 /*Cargrega mapa de nivell*/
 function loadMap() {
   //var mapa = [][];
-  readTextFile("src/mapa-2.txt");
-
-
-
+  //readTextFile("src/mapa-2.txt");
+  readFile();
 }
 
+function readFile () {
+  ajax.request("http://puigpedros.salleurl.edu/pwi/pac4/token/" + token + "/slot/" + slot);
+}
 
+//Ajax asincron
+var ajax = {
+  request: function (url) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", reqListener);
+  xhr.open("POST", url, "src/mapa-2.txt");
+  xhr.send();
+  }
+};
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
+function reqListener () {
+  var items = JSON.parse (this.responseText).items;
+  alert (items);
 }
