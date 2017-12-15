a7201f47de97a6e24ae3c7e78eef46d3e667ce1a
jordiam97 <jordi.alonso.2015@salleurl.edu>
2017-12-15 14:55:12 +0100

    classes wall, door, air

--- js/loadMap.js
+++ js/loadMap.js
@@ -1,9 +1,26 @@
 /*Cargrega mapa de nivell*/
+
+//Classes per les caselles (excepte pels enemics i els jugador)
+var wall = {
+  img:"media/images/dungeon_wall.png",         //Imatge que es mostrarà en la posició
+  action:"Block"                               //Accions de la classe (per exemple: bloquejar usuari, cridar a una funció per iniciar un combat, superar nivell...)
+};
+
+var door = {
+  img:"media/images/dungeon_door.png",
+  action:"Exit"
+};
+
+var air = {
+  img:"media/images/dungeon_step.png",
+  action:null
+};
+
 function loadMap() {
-  //var mapa = [][];
+
   readTextFile("src/mapa-2.txt");
 
-
+  var mapa = [[]];
 
 }
 
