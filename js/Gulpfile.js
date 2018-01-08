3582e7c1f5f2eb75e19a18c041db78d7a48ddb42
jordiam97 <jordi.alonso.2015@salleurl.edu>
2018-01-08 20:39:06 +0100

    gulp

--- js/Gulpfile.js
+++ js/Gulpfile.js
@@ -2,6 +2,21 @@ var gulp = require('gulp');
 var minifyCSS = require('gulp-csso');
 var less = require('gulp-less');
 var htmlmin = require('gulp-html-minifier2');
+var beautify = require('gulp-beautify');
+var imagemin = require('gulp-imagemin');
+
+
+gulp.task('beautify', function() {
+  gulp.src('./css/*.js')
+    .pipe(beautify({indent_size: 2}))
+    .pipe(gulp.dest('build/styles'))
+});
+
+gulp.task('images', function(){
+  return gulp.src('media/images/**/*.+(png)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('build/images'))
+});
 
 gulp.task('html', function(){
   return gulp.src('*.html')
